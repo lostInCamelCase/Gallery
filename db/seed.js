e413d07134e7dc4219d445062660b83cb8118e0f
Michael wu <mwuf15@gmail.com>
2020-08-08 12:35:44 -0700

    eslinted all the files

--- db/seed.js
+++ db/seed.js
@@ -1,49 +1,46 @@
-const db =  require('./index.js');
+const db = require('./index.js');
 const random = require('./infoGeneration.js');
-const faker = require('faker');
 
 const insertInfo = () => {
   // console.log(`${placeName},${rating},${host},${location}`)
-  for (var i = 1; i < 101; i++) {
-    var placeName = random.randomListingName();
-    var location = random.randomLocation();
+  for (let i = 1; i < 101; i++) {
+    const placeName = random.randomListingName();
+    const location = random.randomLocation();
     db.query(`insert into placeToStay (placeName, location) values ("${placeName}", "${location}")`, (err) => {
       if (err) {
         console.log('error insert data into db');
       }
-    })
+    });
   }
 };
 
 const insertRating = () => {
-  for (var i = 1; i < 101; i++) {
-    var reviews = random.randomNum(10,388);
-    var stars = random.randomNum(3,5);
-    var host = random.superHost();
+  for (let i = 1; i < 101; i++) {
+    const reviews = random.randomNum(10, 388);
+    const stars = random.randomNum(3, 5);
+    const host = random.superHost();
     db.query(`insert into ratings (number_Of_Reviews, stars, stay_id, superHost) values ("${reviews}", "${stars}","${i}", "${host}")`, (err) => {
       if (err) {
         console.log('error insert data into db', err);
       }
-    })
+    });
   }
 };
 
 const insertPictures = () => {
-
-  for (var i = 1; i < 101; i++) {
-    for (var j = 0; j < 26; j++) {
-      var picture = random.randomPictures();
-      var caption = random.randomCaptions();
-      db.query(`insert into pictures (images, caption, stay_id) values ("${picture}", "${caption}","${i}")`, (err) => {
+  for (let i = 1; i < 101; i++) {
+    const randomNumberOfPictures = random.randomNum(10, 35);
+    for (let j = 0; j < randomNumberOfPictures; j++) {
+      const picture = random.randomPictures();
+      const caption = random.randomCaptions();
+      db.query(`insert into pictures (images, caption, stay_id) values ("${picture}", "${caption}", "${i}")`, (err) => {
         if (err) {
           console.log('error insert data into db', err);
         }
-      })
+      });
     }
   }
 };
-
-
 
 insertInfo();
 insertRating();
