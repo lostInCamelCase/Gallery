3acc6e8392eaab29edc4b96714c7689d889a47a7
Michael wu <mwuf15@gmail.com>
2020-08-10 01:44:38 -0700

    testing environment setup

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const db = require('../db/index.js')
-const model = require('../db/model.js')
+const model = require('../db/model.js');
+const random = require('../db/infoGeneration.js');
+
 const app = express();
 const PORT = 3008;
 
-app.use(bodyParser.json())
-// Serve static files. Any requests for specific files will be served if they exist in the provided folder
+app.use(bodyParser.json());
+// Serve static files.
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/stay',(req, res) => {
+app.get('/stay', (req, res) => {
   model.getInfo((err, data) => {
     if (err) {
       // console.log('error getting data', err);
@@ -18,25 +19,31 @@ app.get('/stay',(req, res) => {
     } else {
       res.status(200).send(data);
     }
-  })
-})
+  });
+});
 
-app.get('/stay/ratings',(req, res) => {
-  model.getRatings((err, data) => {
+app.get('/stay/ratings', (req, res) => {
+  const id = random.randomNum(1, 100);
+  model.getRatings(id, (err, data) => {
     if (err) {
       // console.log('error getting data', err);
       res.status(404).send();
     } else {
       res.status(200).send(data);
     }
-  })
-})
+  });
+});
 
-
-
-
-
-
-
+app.get('/stay/pictures', (req, res) => {
+  const id = random.randomNum(1, 100);
+  model.getPictures(id, (err, data) => {
+    if (err) {
+      // console.log('error getting data', err);
+      res.status(404).send();
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 // Start the server on the provided port
-app.listen(PORT, () => console.log('Listening on port: ' + PORT));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
