086d9756fe1048502fc799e2b917fecaab4e492f
Michael wu <mwuf15@gmail.com>
2020-08-13 19:25:54 -0700

    added modal

--- client/src/components/GalleryList.jsx
+++ client/src/components/GalleryList.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import GalleryView from './GalleryView.jsx';
+import styles from '../GalleryList.css';
 
-const GalleryList = ({pictures}) => (
-  <div>
-   {/* {pictures.map((image) => (
-     <GalleryView image={image} key={image.id}/>
-   ))} */}
+const GalleryList = ({pictures, show, handleClose}) => {
+  let showHideClassName = show ? "displayBlock" : "displayNone";
+  if (show === false) {
+    return null;
+  }
+  return(
+  <div className={styles.showHideClassName}>
+    <div className={styles.modalMain}>
+    <button onClick={handleClose}>close</button>
+    <br></br>
+      {pictures.map((image) => (
+        <GalleryView image={image} key={image.id} handleClose={handleClose}/>
+      ))}
+    </div>
   </div>
-);
+  )
+};
 
 export default GalleryList;
