83f9c4d8b59dacb7461448b0a92b19b2cffa5cce
Michael wu <mwuf15@gmail.com>
2020-08-15 09:50:19 -0700

    minor eslint changes and added icons

--- client/src/components/GalleryView.jsx
+++ client/src/components/GalleryView.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from '../GalleryView.css';
 
-const GalleryView = ({image, handleClose, idx}) => (
+const GalleryView = ({ image, handleClose, idx }) => (
   <div className={styles.imagesContainer}>
-    <img className={styles.images}src={image[idx].images} ></img>
+    <img className={styles.images} src={image[idx].images} alt="picture gallery"></img>
   </div>
-
 );
 
 export default GalleryView;
