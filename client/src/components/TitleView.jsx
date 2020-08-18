7b6174ccacdada02cfb7c9f5b5a9cf052afe55a4
Michael wu <mwuf15@gmail.com>
2020-08-17 17:01:50 -0700

    conditional rendering on superhost

--- client/src/components/TitleView.jsx
+++ client/src/components/TitleView.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from '../TitleView.css';
 
-const TitleView = ({stayName, rating, showShare, showSave}) => (
-  <div className={styles.title}>
+const TitleView = ({stayName, rating, showShare, showSave}) => {
+
+  if (rating.superHost === 'host') {
+    return (
+      <div className={styles.title}>
     <h2>{stayName.placeName}</h2>
     <div className={styles.rating}>{rating.stars}  ({rating.number_Of_Reviews})</div>
     <div className={styles.star}><i className="fas fa-star"></i></div>
     <div className={styles.medal}><i className="fas fa-medal"></i></div>
-    <div className={styles.host}>{rating.superHost}</div>
-    <div className={styles.location}>{stayName.location}</div>
+    <div className={styles.location2}>{stayName.location}</div>
     <div className={styles.share}>
       <div className={styles.shareIcon}>
         <i className="fas fa-sign-out-alt"></i>
@@ -17,6 +19,24 @@ const TitleView = ({stayName, rating, showShare, showSave}) => (
     </div>
     <div className={styles.save} onClick={showSave}><i className="far fa-heart"></i> Save</div>
   </div>
-);
+  )};
 
+  return (
+    <div className={styles.title}>
+      <h2>{stayName.placeName}</h2>
+      <div className={styles.rating}>{rating.stars}  ({rating.number_Of_Reviews})</div>
+      <div className={styles.star}><i className="fas fa-star"></i></div>
+      <div className={styles.medal}><i className="fas fa-medal"></i></div>
+      <div className={styles.host}>{rating.superHost}</div>
+      <div className={styles.location}>{stayName.location}</div>
+      <div className={styles.share}>
+        <div className={styles.shareIcon}>
+          <i className="fas fa-sign-out-alt"></i>
+        </div>
+        <div className={styles.shareText} onClick={showShare}>Share</div>
+      </div>
+      <div className={styles.save} onClick={showSave}><i className="far fa-heart"></i> Save</div>
+    </div>
+  );
+  }
 export default TitleView;
